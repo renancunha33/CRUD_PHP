8bec5ac18db693d63f7e6a73aa77813efc654301
Renan Cunha <renan.cunha20@gmail.com>
2016-05-03 19:25:44 -0300

    update
    form de atualização só aparece após pesquisa pr id.

--- atualiza.php
+++ atualiza.php
@@ -11,28 +11,29 @@
 	<div class="container">
 		<!-- PHP que procura o registro e exibe campos para alterações-->
 		<?php
-        error_reporting(0);
-        ini_set('display_errors', 0);
-        include 'conecta_mysql.inc';
+		error_reporting(0);
+		ini_set('display_errors', 0);
+		include 'conecta_mysql.inc';
 
-        $ID = $_POST['ID'];
+		$ID = $_POST['ID'];
         //comando sql
-        $reso = mysqli_query($conexao, "select * from cadastroCRUD WHERE id_cadastro = '$ID' ");
+		$reso = mysqli_query($conexao, "select * from cadastroCRUD WHERE id_cadastro = '$ID' ");
         //retorno do comando sql no registro procurado
-        $row = mysqli_fetch_array($reso);
-
+		//$row = mysqli_fetch_array($reso);
+		while ($row = mysqli_fetch_array($reso)) {
         //form criadoo em php que envia os dados para o atualizacao.php processar a atualização
-        echo'REGISTRO ID: '.$ID."<br/>	
-		<form method='POST' action='atualizacao.php'>
+			echo'REGISTRO ID: '.$ID."<br/>	
+			<form method='POST' action='atualizacao.php'>
 
-		<input type='text'name='ID' value='".$row['id_cadastro']."' readonly><br/>
-		<input type='text' name='nome' placeholder='Nome'value='".$row['nm_cadastro']."'><br/>
-		<input type='text' name='telefone' placeholder='Telefone'value='".$row['tel_cadastro']."'><br/>
-		<input type='submit' value='Atualizar' name='submit'>
+			<input type='text'name='ID' value='".$row['id_cadastro']."' readonly><br/>
+			<input type='text' name='nome' placeholder='Nome'value='".$row['nm_cadastro']."'><br/>
+			<input type='text' name='telefone' placeholder='Telefone'value='".$row['tel_cadastro']."'><br/>
+			<input type='submit' value='Atualizar' name='submit'>
 
-		</form>
-		";
-        ?>
+			</form>
+			";
+		}
+		?>
 	</div>
 </body>
 </html>
