0a1a0f0e5d9f863e2f40ee7f63c14b4a145cc695
Renan Cunha <renan.cunha20@gmail.com>
2016-05-19 20:39:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
 <body>
 	<!--form que envia os dados para novo cadastro no cadastrar.php-->
 	<form method= "POST" action="cadastrar.php">
-		<input type="text" class="form-control"name="nome" placeholder="Nome real"value=""><br/>
-		<input type="text" class="form-control"name="telefone" placeholder="Telefone"value=""><br/>
+		<input type="text" class="form-control"name="nome" placeholder="Nome real"value="">
+		<br/>
+		<input type="text" class="form-control"name="telefone" placeholder="Telefone"value="">
+		<br/>
 		<button type="submit" name="enviar">Cadastrar</button>
 	</form>
 	<div class="container">
